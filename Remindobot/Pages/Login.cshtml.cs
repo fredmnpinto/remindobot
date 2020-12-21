94f43cadf81c49f629e4801f93c44fb1c901764a
Frederico Pinto <39822@ufp.edu.pt>
2020-12-21 20:04:24 +0000

    Name Change

--- Remindobot/Pages/Login.cshtml.cs
+++ Remindobot/Pages/Login.cshtml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SecondTry.Pages
+namespace Remindobot.Pages
 {
     public class Login : PageModel
     {
--- Remindobot/Pages/Main.cshtml.cs
+++ Remindobot/Pages/Main.cshtml.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SecondTry.Pages
+namespace Remindobot.Pages
 {
     public class Main : PageModel
     {
